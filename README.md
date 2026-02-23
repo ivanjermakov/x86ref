be85ceb6bdecdcdd5dae147e5fa0a09edc962ba2
ivanjermakov <ivanjermakov1@gmail.com>
2026-02-23 18:39:10 +0100

    Init

---
+++ README.md
@@ -0,0 +1,7 @@
+# x86ref
+
+x86 opcode and instruction reference page
+
+# Credits
+
+- [Reference data](https://github.com/mazegen/x86reference)
---
+++ build.ts
@@ -0,0 +1,30 @@
+import { existsSync } from "fs";
+import { load } from "cheerio";
+import { cp, mkdir, readFile, writeFile } from "fs/promises";
+
+await mkdir("build", { recursive: true });
+await mkdir("cache", { recursive: true });
+
+const refHtmlCached = "cache/coder64.html";
+
+let refHtml: string;
+if (existsSync(refHtmlCached)) {
+	refHtml = (await readFile(refHtmlCached)).toString();
+} else {
+	const res = await (
+		await fetch("http://ref.x86asm.net/coder64.html")
+	).arrayBuffer();
+	refHtml = new TextDecoder("ISO-8859-1").decode(res);
+	await writeFile(refHtmlCached, refHtml);
+}
+const refDocQuery = load(refHtml);
+const table1 = refDocQuery("table.ref_table").eq(0);
+
+const template = (await readFile("index.html")).toString();
+const out = load(template);
+out("#table").replaceWith(table1);
+
+const outPath = "build/index.html";
+await writeFile(outPath, out.html());
+
+await Promise.all(["index.js", "index.css"].map((f) => cp(f, `build/${f}`)));
